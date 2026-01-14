d61f72dad1e6007a1cc1c1738114aa84bad59122
Gourav Devrale <devralegourav@gmail.com>
2026-01-14 13:26:51 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// 1. . Write a function called countOccurrences()
+// that takes in a string and a character and returns
+// the number of occurrences of that character in the string.
+
+
+
+// 2. Write a function called findMaxNumber that takes
+// in an array of numbers and returns the largest number
+// in the array.
+
+
+
+// 3. Write a function called titleCase that takes in a string
+// and returns the string with the first letter of each word
+// capitalized.
+
+
+
+
+// 4. Write a function called reverseString that takes in a
+// string and returns the reverse of that string.
+
+
+// 5. Write a function called countVowels that takes in a
+//  string and returns the number of vowels in the string.
+
+
+
+// 6. Write a function called removeDuplicates that takes in
+// an array and returns a new array with duplicates
+// removed.
+
+
+// 7. A palindrome is a word, phrase, number, or other
+// sequence of characters which reads the same backward or
+// forward. An example of a palindrome is "madam".
